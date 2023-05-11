8c4fd40d564fbb9c6c7fdaca7cd639fdf0fead29
stewlove <lovell.stewart@student.greenriver.edu>
2023-05-11 18:48:12 +0000

    test

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World must restart container to see changes');
+  res.send('fuck you twice');
 });
 
 app.listen(PORT, HOST, () => {
