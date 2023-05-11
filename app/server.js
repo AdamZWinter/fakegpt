c2bc1e66019874d0b7a5dffcf162b7c99ec92269
AdamZWinter <adam@bootstrapmusic.com>
2023-05-11 18:12:37 +0000

    compose files for everyone

--- app/server.js
+++ app/server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World cannot edit while running');
+  res.send('Hello World must restart container to see changes');
 });
 
 app.listen(PORT, HOST, () => {
