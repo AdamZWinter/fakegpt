dd0a42b238b11d009c47915802560b8a007edc46
adamandwise <awise.developer@gmail.com>
2023-05-19 07:43:06 +0000

    updated backend code to support json object created during transcript research, it now behaves like a tutor teaching  a lesson about the cival war

--- app/server.js
+++ app/server.js
@@ -21,13 +21,33 @@ const HOST = '0.0.0.0';
 app.use(express.json());
 //app.use(cors());
 
+let systemRole = `{
+    "what is this json object?" : "prompt instructions for how I want you to speak to me",
+    "relationship" : "this is a mentor-mentee relationship",
+    "AI_language_model_as_mentor": "Socrates-like, never explicitly state you are an ai model",
+    "mentee" : " refer to me as adam",
+    "mentee_age" : "13-18",
+    "response_level": "conversational",
+    "learning_approach":
+    { 
+        "use_analogies": true,
+        "ask_questions": true, 
+        "dialogue_based": true
+    },
+    "Initial prompt": ""
+}`;
+
 let history = [
-    {role:"system", content: "You are a cow. You reply with moo to every message."}
+    {role:"system", content: systemRole},
+    {role:"user" , content:"Greet me with a lesson plan about the American Cival War."}
 ];
 
 app.post("/completions", async (req, res) => {
+
     console.log("Request body message: " + req.body.message);
+
     history.push({ role: "user", content: req.body.message });
+
     console.log(history)
     const options = {
         method: "POST",
@@ -59,28 +79,50 @@ app.get("/completions", async (req, res) => {
     })
 });
 
-app.post("/", async (req, res) => {
-    //console.log(history)
+// app.post("/", async (req, res) => {
+//     //console.log(history)
 
-    const { messages } = req.body;
+//     const { messages } = req.body;
 
     
-    console.log(messages);
-    const  completion = await openai.createChatCompletion({
-        model:"gpt-3.5-turbo",
-        messages:[
-            {role:"system", content: "You are TutorGPT. You like to give analogies when explaining concepts, and ask follow up questions to reframe and enhance my own thinking. "},
-            ...messages
-            //{role:"user", content: `${message}`},
-        ]
-    })
+//     console.log(messages);
+//     const  completion = await openai.createChatCompletion({
+//         model:"gpt-3.5-turbo",
+//         messages:[
+//             {role:"system", content: "You are TutorGPT. You like to give analogies when explaining concepts, and ask follow up questions to reframe and enhance my own thinking. "},
+//             ...messages
+//             //{role:"user", content: `${message}`},
+//         ]
+//     })
 
-    res.json({
-        completion: completion.data.choices[0].message
-    })
-});
+//     res.json({
+//         completion: completion.data.choices[0].message
+//     })
+// });
 
 app.get("/", async (req, res) => {
+    const options = {
+        method: "POST",
+        headers:{
+            "Authorization": `Bearer ${api_key}`,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            model : "gpt-3.5-turbo",
+            messages: history,
+            max_tokens: 100
+        })
+    }
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+        const data = await response.json();
+        console.log(data)
+        history.push({ role: "assistant", content: data.choices[0].message.content });
+        console.log(history)
+        res.send(data.choices[0].message);
+    } catch (error) {
+        console.error(error);
+    }
     res.json({
         response: "The backend is up and running, but you cannot use it this way.  You must POST a message body?"
     })
@@ -88,7 +130,7 @@ app.get("/", async (req, res) => {
 
 app.get('/reset', (req, res) => {
     history = [
-        {role: 'system', content: 'You are TutorGTP. You are designed to be a tutor and like to give analogies when explaining concepts, and you reframe and ask follow up questions to enhance the student experience.'}
+        {role: 'system', content: systemRole}
     ];
     res.send('History reset');
 });
